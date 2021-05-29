573668609abc5a5607860b27bf001503a586d162
Bakhtiyar <bakhtiyartayirov@gmail.com>
2021-05-29 14:21:20 +0500

    1.2. Входные данные в тесте [#297750]

---
+++ src/ru/job4j/condition/X2.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class X2 {
+    public static int calc(int a, int b, int c, int x) {
+        return a * x * x + b * x + c;
+    }
+
+    public static void main(String[] args) {
+        int a = 1, b = 1, c = 1, x = 1;
+        int rsl = X2.calc(a, b, c, x);
+        System.out.println(rsl);
+    }
+}
